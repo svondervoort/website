830ffc77e4fb89dafcb8726188fef969803f815f
Sander <s.vdvondervoort@lameco.nl>
2023-02-27 13:33:48 +0100

    Import tailwind from app.js instead of globals.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+// import '../styles/globals.css';
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
